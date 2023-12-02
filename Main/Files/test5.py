3a08d4964600b0bcf72cd4a5a0a6865da67e17c4
AquaGn0me <93402564+AquaGn0me@users.noreply.github.com>
2023-12-02 15:36:54 -0500

    lines

--- Main/Files/test5.py
+++ Main/Files/test5.py
@@ -1,10 +1,10 @@
 import random, pygame, pygame.gfxdraw
 
 # simulation variables
-dt = 0.1
+dt = 0.05
 damping = 1
 width, height = (1500, 700)
-ballmax = 100
+ballmax = 4
 
 class Object():
     
@@ -31,20 +31,22 @@ class Object():
         self.KE = 1/2 * (self.velx**2 + self.vely**2)
 
         # checking for floor and bounce
-        if self.y >= height - self.radius:
+        if self.y > height - self.radius:
 
             self.vely = self.vely * -1 * damping
-            #self.display()
+            self.y = height - self.radius
 
-        elif self.x <= 0 + self.radius:
+        elif self.x < self.radius:
             self.velx = self.velx * -1 * damping
-            #self.display()
+            self.x = self.radius
 
-        elif self.x >= width - self.radius:
+        elif self.x > width - self.radius:
             self.velx = self.velx * -1 * damping
+            self.x = width - self.radius
         
-        elif self.y <= self.radius:
+        elif self.y < self.radius:
             self.vely = self.vely * -1 * damping
+            self.y = self.radius
 
             #self.display()
         else:
@@ -60,6 +62,21 @@ class Object():
             Kinetic Energy: {round(self.KE, 3)}
             Potential Energy: {round(self.PE, 2)}
                 """)
+    def drawEnergy(self, screen):
+        pygame.draw.line(screen, (255,0,0), (5,height), (5, (height - round(100*self.KE/13000))), 10)
+        pygame.draw.line(screen, (0,255,0), (16,height), (16, (height - round((800/685)*self.PE))), 10)
+
+    def connect(self, screen, balllist, number):
+        
+        for l in range(1, ballmax-1):
+            if number == l:
+                pass
+
+            else:
+                pygame.draw.line(screen, (0,0,0), (round(self.x), round(self.y)), (round(balllist[l].x), round(balllist[l].y)), round((((self.x - self.y)**2 + (balllist[l].x - balllist[l].y)**2)**0.5)/800)*10+1)
+                print(round((((self.x - self.y)**2 + (balllist[l].x - balllist[l].y)**2)**0.5)))
+                
+        
 
 def main():
 
@@ -74,7 +91,8 @@ def main():
 
     balllist = []
     for i in range(1,ballmax):
-        balllist.append(Object(i, (random.randint(0,254),0,0), random.randint(100,900), random.randint(10,400), random.randint(-100,100), 0, 10, random.randint(10,25)))
+        balllist.append(Object(i, (random.randint(0,254),0,0), random.randint(5, 1485), random.randint(115,685), random.randint(-100,100), 0, 10, 3))
+        
 
     # main loop
     running = True
@@ -90,15 +108,16 @@ def main():
                     running = False
 
         # main clock for the simulation
-        clock.tick(60)
+        clock.tick(144)
 
         screen.fill((255,255,255))
 
         # update loop
         for i in range(1,ballmax - 1):
-            pygame.gfxdraw.filled_circle(screen, round(balllist[i].x), round(balllist[i].y), balllist[i].radius, (round(255*balllist[i].KE/15000),0,0))
+            pygame.gfxdraw.filled_circle(screen, round(balllist[i].x), round(balllist[i].y), balllist[i].radius, (round(255*balllist[i].KE/13000),0,0))
             balllist[i].posupdate()
             balllist[i].velupdate()
+            balllist[i].connect(screen, balllist, i)
         
         pygame.display.update()
 
